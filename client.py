746f55213309428435ee223cbe25a8cbacbaef36
Daniel <mokapoop24@gmail.com>
2026-04-28 15:01:37 +0300

    codefix

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 
-SERVER_ADDR = "10.100.102.8"
+SERVER_HOSTNAME = socket.gethostname()
 SERVER_PORTS = { "VIDEO": 8001, "SOUND": 8002, "INPUT": 8003 }
 
 def main():
     clientVideoSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serverVideoSocketAddr = (socket.gethostname(), SERVER_PORTS["VIDEO"]) 
+    serverVideoSocketAddr = (SERVER_HOSTNAME, SERVER_PORTS["VIDEO"]) 
 
     clientVideoSocket.sendto(bytes("[client][request] ==> Send data please! I'm on my hands and knees!!", "utf-8"), serverVideoSocketAddr)
     data, addr = clientVideoSocket.recvfrom(65535)
